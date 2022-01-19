5bdf0c888efb672560328063f6771e2e96f51e32
sarahelenz <selenz3112@stu.d214.org>
2022-01-19 13:32:58 -0600

    Initial commit

---
+++ src/ArrayLecture.java
@@ -0,0 +1,15 @@
+public class ArrayLecture {
+    public static void main(String[] args) {
+        int array[] = new int[5];
+        System.out.println(array[0]);
+        String[] array2 = new String[5];
+        System.out.println(array[2]);
+        array2[1] = "hello";
+
+        int array3[] = {1,2,3};
+
+        array = new int[10];
+
+     // Can't do:   array = {1,2,3};
+    }
+}
