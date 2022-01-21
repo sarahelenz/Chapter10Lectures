b0a44d84598adb5b7f23b9b4a1d7c7b2f93980ff
sarahelenz <selenz3112@stu.d214.org>
2022-01-21 13:31:36 -0600

    added loops to WarmUp10_1

---
+++ src/ArraysWithLoops.java
@@ -0,0 +1,9 @@
+public class ArraysWithLoops {
+    public static void main(String[] args) {
+        int array[] = {5,2,7,9,3};
+
+        for(int x = 0; x < array.length; x++){
+            System.out.println(array[x]);
+        }
+    }
+}
--- src/WarmUp10_1.java
+++ src/WarmUp10_1.java
@@ -10,15 +10,33 @@ public class WarmUp10_1 {
 	public static void main(String[] args) {
 		int array[] = new int[4];
 		array[0] = 1; array[1] = 5; array[2] = 7; array[3] = 3;
-		System.out.println("[" + array[0] + "," + array[1] + "," + array[2] + "," + array[3] + "]");
+//		System.out.println("[" + array[0] + "," + array[1] + "," + array[2] + "," + array[3] + "]");
 //		array[0] = array[0] + 1;
 //		array[1] = array[1] + 1;
 //		array[2] = array[2] + 1;
 //		array[3] = array[3] + 1;
+		System.out.print("[");
+		for(int x = 0; x <= array.length - 1; x++){
+			System.out.print(array[x]);
+			if(x < array.length - 1){
+				System.out.print(",");
+			}
+		}
+		System.out.println("]");
+
 		for(int x = 0; x <= array.length - 1; x++){
 			array[x] += 1;
 		}
-		System.out.println("[" + array[0] + "," + array[1] + "," + array[2] + "," + array[3] + "]");
+
+		System.out.print("[");
+		for(int x = 0; x <= array.length - 1; x++){
+			System.out.print(array[x]);
+			if(x < array.length - 1){
+				System.out.print(",");
+			}
+		}
+		System.out.print("]");
+//		System.out.println("[" + array[0] + "," + array[1] + "," + array[2] + "," + array[3] + "]");
 
 	}
 
