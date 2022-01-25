0a6221129b498019afc2118ac57b6237d2155a2e
sarahelenz <selenz3112@stu.d214.org>
2022-01-25 12:54:38 -0600

    added notes to lecture

--- src/ArraysWithMethods.java
+++ src/ArraysWithMethods.java
@@ -1,14 +1,16 @@
 public class ArraysWithMethods {
     public static void main(String[] args) {
-       // int array[] = new int[2];
+       // int array[] = new int[2]; array[0] = 5; array[1] = 7;
         int array[] = {5, 7};
         method(array);
     }
 
     public static void method(int a[]){
+        //Changing values of array
         for(int x = 0; x < a.length; x++) {
             a[x] += 2;
         }
+        //Printing whole array
         System.out.print("{");
 
         for(int x = 0; x < a.length; x++){
