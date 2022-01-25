9f70614ec45304aa311ad0b1781c7dbb8f41f33b
sarahelenz <selenz3112@stu.d214.org>
2022-01-25 12:30:20 -0600

    Added notes to a lecture

--- src/ArrayLecture.java
+++ src/ArrayLecture.java
@@ -2,14 +2,14 @@ public class ArrayLecture {
     public static void main(String[] args) {
         int array[] = new int[5];
         System.out.println(array[0]);
-        String[] array2 = new String[5];
+        String[] array2 = new String[5]; //put the brackets with the data type to make multiple variables of the array
         System.out.println(array[2]);
         array2[1] = "hello";
 
         int array3[] = {1,2,3};
-
+        //Creates a new array and the other array won't be able to be accessed
         array = new int[10];
 
-     // Can't do:   array = {1,2,3};
+     // Can't do:   array = {1,2,3}; since it was already created
     }
 }
