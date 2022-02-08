ad685b3b137b37f6f240176f59bf60793cc690b0
sarahelenz <selenz3112@stu.d214.org>
2022-02-08 13:33:04 -0600

    added WarmUp10_4

--- src/TwoDimensionalArrays.java
+++ src/TwoDimensionalArrays.java
@@ -2,9 +2,9 @@ public class TwoDimensionalArrays {
     public static void main(String[] args) {
         int[][] array2D = new int[3][4];
         int[][] array2DSequel = {{1,2}, {3,4,5}};
-        int[][] array3 = {{1,2,3}, {4,5,6}};
-
-     //   method(array3[0]);
+        int[][] array3 = {{3,2,1}, {4,5,6}};
+        printArray(sortArray(array3));
+         //   method(array3[0]);
 
         array2D[0][0] = 5;
 
@@ -14,6 +14,31 @@ public class TwoDimensionalArrays {
             }
             System.out.println();
         }
+    }
+    public static void printArray(int[][] a){
+
+        for(int x = 0; x < a.length; x++){
+            System.out.print("[ ");
+            for(int i = 0; i <= a.length; i++){
+                System.out.print( a[x][i]);
+                if(i < a.length){
+                    System.out.print(", ");
+                }
+            }
+            System.out.println(" ]");
+        }
+    }
+    public static int[][] sortArray(int[][] a){
+        for(int x = 0; x < a.length; x++){
+            for(int i = 0; i < a.length - 1; i++){
+                if(a[x][i] > a[x][i + 1]){
+                    int temp = a[x][i];
+                    a[x][i] = a[x][i + 1];
+                    a[x][i + 1] = temp;
+                }
+            }
+        }
+        return a;
     }
     //method for one row
 //    public static int[][] method(int a[]){
